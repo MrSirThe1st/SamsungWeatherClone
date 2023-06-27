2e521b5502453938dcef9d9030739d0e504699dc
MrSirThe1st <marcilungambuyu@gmail.com>
2023-06-27 23:38:40 +0200

    api fetching integration

--- SamsungWeather/api/weatherApi.js
+++ SamsungWeather/api/weatherApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { apiKey } from '../constants';
 
 const forecastEndpoint = params=> `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${params.cityName}&days=${params.days}&aqi=no&alerts=no`;
-const locationEndpoint = params=> `https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${params.cityName}`;
+const locationsEndpoint = params=> `https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${params.cityName}`;
 
 const apiCall = async (endpoint)=>{
     const options = {
@@ -16,4 +16,12 @@ const apiCall = async (endpoint)=>{
         console.log('error')
         return null;
     }
+}
+
+export const fetchWeatherForecast = params=>{
+    return apiCall(forecastEndpoint(params));
+}
+
+export const fetchLocations = params=>{
+    return apiCall(locationsEndpoint(params));
 }
